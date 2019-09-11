1561febfcaa80f4f66687166749e1c5519e6e38d
CodesDaniel <mailidanielifechukwu@gmail.com>
2019-09-12 00:30:03 +0100

    Added Browser Start test

--- test/header.test.js
+++ test/header.test.js
@@ -1,5 +1,16 @@
+ const puppeteer = require('puppeteer')
 
 test('Adds to numbers ', () => {
      const sum = 4+1;
      expect(sum).toEqual(5);
 })
+
+test('Does browser start', async () => {
+    const  browser =  await puppeteer.launch({
+     headless:false
+    })
+    const page = await browser.newPage()
+    await page.goto("http://localhost:3000")
+    
+}, 10000)
+

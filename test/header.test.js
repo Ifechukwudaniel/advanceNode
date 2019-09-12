f3d586fb1249c4492df28f39c7b558474dbc8933
CodesDaniel <mailidanielifechukwu@gmail.com>
2019-09-12 07:57:13 +0100

    Started writing testing for oauth

--- test/header.test.js
+++ test/header.test.js
@@ -1,16 +1,25 @@
  const puppeteer = require('puppeteer')
+ let browser , page
 
-test('Adds to numbers ', () => {
-     const sum = 4+1;
-     expect(sum).toEqual(5);
-})
-
-test('Does browser start', async () => {
-    const  browser =  await puppeteer.launch({
-     headless:false
+ beforeEach( async ()=>{
+    browser =  await puppeteer.launch({
+        headless:false
     })
-    const page = await browser.newPage()
+    page = await browser.newPage()
     await page.goto("http://localhost:3000")
-    
+ })
+
+ afterEach( async ()=>{
+     await browser.close()
+ })
+
+test('The header has the correct text', async () => {
+    const text = await page.$eval("a.left.brand-logo", el=> el.innerHTML)
+    expect(text).toEqual('Blogster')
 }, 10000)
 
+test('clicking login hs oauth flow', async () => {
+     await page.click((".right a"))
+     const url = page.url()
+     expect(url).toMatch(/accounts\.google\.com/)
+})

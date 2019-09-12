ff76690a68e8934b2331c6bc54b8c3ec2e538fff
CodesDaniel <mailidanielifechukwu@gmail.com>
2019-09-12 14:16:33 +0100

    Started Login test

--- test/header.test.js
+++ test/header.test.js
@@ -1,25 +1,45 @@
  const puppeteer = require('puppeteer')
  let browser , page
+ const url = "http://localhost:3000"
 
  beforeEach( async ()=>{
     browser =  await puppeteer.launch({
         headless:false
     })
     page = await browser.newPage()
-    await page.goto("http://localhost:3000")
+    await page.goto(url)
  })
 
  afterEach( async ()=>{
-     await browser.close()
+   await browser.close()
  })
 
 test('The header has the correct text', async () => {
     const text = await page.$eval("a.left.brand-logo", el=> el.innerHTML)
     expect(text).toEqual('Blogster')
-}, 10000)
+}, 100000)
 
-test('clicking login hs oauth flow', async () => {
+test('clicking login has oauth flow', async () => {
      await page.click((".right a"))
      const url = page.url()
      expect(url).toMatch(/accounts\.google\.com/)
 })
+test.only('When signed in,has logout button',  async() => {
+ const userId = '5d78249787c5f5cc646c08f5'
+ const Buffer = require("safe-buffer").Buffer
+ const keyGrip = require('keygrip')
+ const key = require("../config/keys")
+
+ const sessionObject = {
+     passport:{
+         user:userId
+     }
+ } 
+  const sessionString =  Buffer.from(JSON.stringify(sessionObject)).toString("base64")
+  const keygrip = new keyGrip([key.cookieKey])
+  const sign = keygrip.sign(`session=${sessionString}`)
+  await page.setCookie({ name:'session', value: sessionString})
+  await page.setCookie({name: 'session.sig', value:sign })
+  await page.goto(url)
+
+}, 100000)
